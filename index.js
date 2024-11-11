c2f8cf79a1fb844a76a74e9c01472ffc96da7239
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-11 22:58:17 +0600

    added

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const sendEmail = (emailAddress, emailData) => {
   })
 
   // verify transporter
-  // verify 
+  // verify
   transporter.verify(function (error, success) {
     if (error) {
       console.log(error)
