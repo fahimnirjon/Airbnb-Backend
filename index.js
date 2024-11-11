879cdb72a1f157f1ba603f91660017afc53a296d
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-11 22:24:24 +0600

    added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8000;
 
 // middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://comforth-be903.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
