4f0f79e51bfb0ffc7b21772a10b1f79c6c4d88ef
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-11 23:01:53 +0600

    added

--- index.js
+++ index.js
@@ -474,7 +474,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello from StayVista Server..");
+  res.send("Hello from StayVist Server..");
 });
 
 app.listen(port, () => {
