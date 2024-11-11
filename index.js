7ed57c94fb23ff114d092e8940b004803a882701
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-11 23:02:26 +0600

    added

--- index.js
+++ index.js
@@ -474,7 +474,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello from Sta Server..");
+  res.send("Hello from St Server..");
 });
 
 app.listen(port, () => {
